3299a6f64dcbcb8fd4ee914d27aef088eca32816
DESKTOP-6GNSTCE\valc_ <alexcaron880@gmail.com>
2018-01-14 17:50:21 -0500

    car done

--- CarCollision/Program.cs
+++ CarCollision/Program.cs
@@ -7,6 +7,12 @@ using System.Threading;
 
 namespace CarCollision
 {
+    public class BrickPos
+    {
+        public int HeightClass { get; set; }
+        public int LaneClass { get; set; }
+        public int CompteurClass { get; set; }
+    }
     class Program
 
 
@@ -63,11 +69,24 @@ namespace CarCollision
             }
         }
 
-        static void BrickWallRender(ref List<int> LHeight, ref List<int> LLane)
+        static void BrickWallRender(ref List<BrickPos> Bricks, int Difficulty)
         {
-            int ChancesOfHappeing = Rand.Next(0, 5);
-            // if (ChancesOfHappeing == 0)
+            int ChancesOfHappeing = 0;
+
+            if (Difficulty == 1)
+            ChancesOfHappeing = Rand.Next(0, 5);
+            if (Difficulty == 2)
+            ChancesOfHappeing = Rand.Next(0, 4);
+            if (Difficulty == 3)
+            ChancesOfHappeing = Rand.Next(0, 3);
+            if (Difficulty == 4)
+            ChancesOfHappeing = Rand.Next(0,2);
+            if (Difficulty == 5)
+            ChancesOfHappeing = 0;
+
+            if (ChancesOfHappeing == 0)
             {
+                int Compteur = 0;
                 int Height = -5;//Rand.Next(0, 30);
                 int RightLane = 1; // a 4
                 int LeftLane = 6; // a 10
@@ -77,74 +96,84 @@ namespace CarCollision
                 else
                     Side = LeftLane;
 
-               // Console.SetCursorPosition((Side), Console.CursorTop = Height);
+                // Console.SetCursorPosition((Side), Console.CursorTop = Height);
                 //Console.WriteLine("----");
-                LHeight.Add(Height);
-                LLane.Add(Side);
-            }
+
+                //cree les cartes avec type i et nomcarte j dans mes enums  //cree les cartes avec type i et nomcarte j dans mes enums
+                Bricks.Add(new BrickPos() { HeightClass = Height, LaneClass = Side , CompteurClass = Compteur });
+
+                /*
+                Bricks[Bricks.Count - 1].HeightClass = Height;
+                Bricks[Bricks.Count - 1].LaneClass = Side;
+                    Bricks[Bricks.Count -1].CompteurClass = Compteur;
+            */
+                }
             //else
             {
                 //fuck off yen a pas
             }
         }
 
-        static void OldBrickRenderDec(ref List<int> LHeight, ref List<int> LLane)
+        static void OldBrickRenderDec(ref List<BrickPos> Pos)
         {
-            if (LHeight.Count != 0)
+            if (Pos.Count != 0)
             {
 
-                for (int x = 0; x < LHeight.Count; ++x)
+                foreach (CarCollision.BrickPos p in Pos.ToList())
                 {
-                    LHeight[x] = (LHeight[x]) + 1;
-                    if ((LHeight[x]) > 29)
+                    p.CompteurClass = (p.CompteurClass) + 1;
+                    p.HeightClass = (p.HeightClass) + 1;
+                    if ((p.HeightClass) > 29)
                     {
-                        LHeight.RemoveAt(x);
-                        LLane.RemoveAt(x);
+                        Pos.Remove(p);
                     }
-                    else if (LHeight[x] > 2 & LHeight[x] <30)
+                    else if (p.HeightClass > 2 & p.HeightClass < 30)
                     {
-                        Console.SetCursorPosition(LLane[x], Console.CursorTop = LHeight[x]);
+                        Console.SetCursorPosition(p.LaneClass, Console.CursorTop = p.HeightClass);
                         Console.WriteLine("----");
                     }
                 }
             }
         }
-        static void DoubleBrickTooClose(bool CarLane, List<int> Height, List<int> Lane)
+        static void DoubleBrickTooClose(ref List<BrickPos> Pos)
         {
-            if (Height.Count > 5)
-            {
-                for (int h = 0; h < (Height.Count -4) ; ++h)
+            //int deleted = 0;
+            foreach (CarCollision.BrickPos p in Pos.ToList())
+                if (p.CompteurClass < 5 & Pos.Count > 2)
                 {
-                    for (int hall = 1; hall < (Height.Count - 4); ++hall)
-                    {
-
-                        if (((Height[h]) - 1 == Height[hall] | (Height[h] - 2) == Height[hall] ) & (Lane[h] != Lane[hall]))
+                    foreach (CarCollision.BrickPos p2 in Pos.ToList())
+                        if (((p.HeightClass) - 1 == p2.HeightClass | (p.HeightClass) - 2 == p2.HeightClass) & (p.LaneClass != p2.LaneClass))
                         {
                             int LR = Rand.Next(1, 3);
                             if (LR == 1)
                             {
-                                Height.RemoveAt(h);
-                                Lane.RemoveAt(h);
+                                Pos.Remove(p);
+                                //deleted = 1;
+                                break;
                             }
                             else
                             {
-                                Height.RemoveAt(hall);
-                                Lane.RemoveAt(hall);
+                                Pos.Remove(p2);
+                                //deleted = 1;
+                                break;
                             }
 
                         }
-                        else if (((Height[h]) - 1 == Height[hall] | (Height[h] - 2) == Height[hall] ) & (Lane[h] == Lane[hall]))
+                        else if (((p.HeightClass) - 1 == p2.HeightClass | (p.HeightClass) - 2 == p2.HeightClass) & (p.LaneClass == p2.LaneClass))
                         {
 
                             {
-                                Height.RemoveAt(hall); //unstable ass part
-                                Lane.RemoveAt(hall);
+                                Pos.Remove(p2);
+                                //deleted = 1;
+                                break;
                             }
 
+
                         }
-                    }
                 }
-             }
+            //if (deleted == 1)
+              //  DoubleBrickTooClose(ref Pos);
+
         }
         static void CarDirectSwitch(ref bool Key) // Direction Switch
         {
@@ -160,32 +189,105 @@ namespace CarCollision
             }
         }
 
-        static void OverlapCheck(bool CarLane, List<int> Height, List<int> Lane)
+        static void OverlapCheck(bool CarLane, List<BrickPos> Pos,int Score)
         {
 
-            for (int x = 0; x < Height.Count; ++x)
+            foreach(CarCollision.BrickPos p in Pos.ToList())
             {
-                if (Height[x] > 26 & Height[x] < 29)
+                if (p.HeightClass > 26 & p.HeightClass < 29)
                 {
-                    if (Lane[x] == 6 & CarLane == true)
+                    if (p.LaneClass == 6 & CarLane == true)
                     {
-                        GameOver();
+                        GameOver(Score);
                         Console.ReadLine();
                         Environment.Exit(1);
                     }
-                    else if (Lane[x] == 1 & CarLane == false)
+                    else if (p.LaneClass == 1 & CarLane == false)
                     {
-                        GameOver();
+                        GameOver(Score);
                         Console.ReadLine();
                         Environment.Exit(1);
                     }
                 }
             }
         }
-
-
-        static void GameOver()
+        static void IntroQuestions(ref int Speed, ref int Difficulty)
         {
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(@"_________                       _________");
+            Console.WriteLine(@"__  ____/ _____ ________        __   ____/ _____ _______ ________");
+            Console.WriteLine(@"_  /     _  __ ` / _  ___ /     _ /   __ _  __  `/_  __`__ \  _  \");
+            Console.WriteLine(@"/ / ___  / /_ / / _   /          / /_/ / / /_/  /_  / / / / / __ /");
+            Console.WriteLine(@"\____ /  \__,_ / / _ /           \____/  \__,_ / / / /_/ /_/\___/");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Difficulty Level : 1- Very Easy          Speed : S - Slow");
+            Console.WriteLine("                   2- Easy                       N - Normal");
+            Console.WriteLine("                   3- Normal                     F - Fast");
+            Console.WriteLine("                   4- Hard                       R - Ridonculous");
+            Console.WriteLine("                   5- Very Hard ");
+            //while (Difficulty == 0 & Speed == 0)
+            do
+            {
+                
+                    
+                    var  THEKEY = Console.ReadKey();
+
+                    switch (THEKEY.Key)
+                    {
+                        case ConsoleKey.D1:
+                            Difficulty = 1;
+                            Console.WriteLine();
+                            Console.WriteLine("Difficulty Set ! Very Easy!");
+                            break;
+                        case ConsoleKey.D2:
+                            Difficulty = 2;
+                            Console.WriteLine("Difficulty Set ! Easy!");
+                            break;
+                        case ConsoleKey.D3:
+                            Difficulty = 3;
+                            Console.WriteLine("Difficulty Set ! Normal!");
+                            break;
+                        case ConsoleKey.D4:
+                            Difficulty = 4;
+                            Console.WriteLine("Difficulty Set ! Hard!");
+                            break;
+                        case ConsoleKey.D5:
+                            Difficulty = 5;
+                            Console.WriteLine("Difficulty Set ! Very Hard!");
+                            break;
+
+                        case ConsoleKey.S:
+                            Speed = 400;
+                            Console.WriteLine("Speed Set ! Slow!");
+                            break;
+                        case ConsoleKey.N:
+                            Speed = 200;
+                            Console.WriteLine("Speed Set ! Normal!");
+                            break;
+                        case ConsoleKey.F:
+                            Speed = 100;
+                            Console.WriteLine("Speed Set ! Fast!");
+                            break;
+                        case ConsoleKey.R:
+                            Speed = 25;
+                            Console.WriteLine("Speed Set ! Ridonculous!");
+                            break;
+                    }
+                
+            } while (Difficulty == 0 | Speed == 0);
+
+
+        }
+
+        static void GameOver(int score)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+
             Console.WriteLine(@"  ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ▒█████   ██▒   █▓▓█████  ██▀███  ");
             Console.WriteLine(@" ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀    ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒");
             Console.WriteLine(@"▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███      ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒");
@@ -206,33 +308,43 @@ namespace CarCollision
             Console.WriteLine(@"   ░   ░ ░  ▒ ░░ ░   ░   ░   ▒    ░░░ ░ ░  ░ ░  ░     ░                  ");
             Console.WriteLine(@"         ░  ░        ░       ░  ░   ░        ░     ░                     ");
             Console.WriteLine(@"                                           ░                        ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"SCORE : {score}");
+
         }
 
         // var statiques
         static bool LeftRight;
-        static List<int> BrickPosHeight;
-        static List<int> BrickPosLane;
+        static List<BrickPos> BrickPosition;
 
         static void Main(string[] args)
         {
             // dec var
             LeftRight = false;
-            BrickPosHeight = new List<int>();
-            BrickPosLane = new List<int>();
+            BrickPosition = new List<BrickPos>();
+            int Speed = 0;
+            int Difficulty = 0;
+
+            IntroQuestions(ref Speed, ref Difficulty);
+
+
+
             int Road = 10000000;
+
 
             for (int x = 0; x < Road; ++x)
             {
                 RoadRender(x);
-                
-                OldBrickRenderDec(ref BrickPosHeight, ref BrickPosLane); // old brick goes down one
-                BrickWallRender(ref BrickPosHeight, ref BrickPosLane);  // 
+
+                OldBrickRenderDec(ref BrickPosition); // old brick goes down one
+                BrickWallRender(ref BrickPosition , Difficulty);  // 
                 KeyMachinRightLeft(ref LeftRight);
                 CarDirectSwitch(ref LeftRight);
-                Thread.Sleep(500);
+                Thread.Sleep(Speed);
                 Console.Clear();
-                DoubleBrickTooClose(LeftRight, BrickPosHeight, BrickPosLane);
-                OverlapCheck(LeftRight, BrickPosHeight, BrickPosLane);
+                DoubleBrickTooClose(ref BrickPosition);
+                OverlapCheck(LeftRight, BrickPosition, x);
             }
         }
     }
